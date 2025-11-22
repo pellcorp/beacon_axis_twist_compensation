2ce010374d6771834b4d6619e947f879bb16f9f9
Jason Pell <jason@pellcorp.com>
2025-11-23 09:13:11 +1100

    Jp k1 support (#1)
    * remove the graph stuff with scipy and just do compensation
    
    * aggressive cleanup and change config to match axis twist so its a matter of copy and paste
    
    * adding x and y axis
    
    * cleanups
    
    * move to root and further cleanup
    
    * fix up installer
    
    * significant cleanups and copying of logic from axis twist compensation
    
    * we only need the delta nothing else and apply the avg thing from axis twist compensation
    
    add homing and heating nozzle to readme

--- README.md
+++ README.md
@@ -1,85 +1,65 @@
-# Gio's Gantry Twist Utility
+# Beacon Axis Twist Compensation
 
-Klipper module to visualize and adjust gantry twist for printers running the Beacon Eddy Current Surface Scanner
+Klipper module adjust axis twist compensation for printers running the Beacon Eddy Current RevH model with contact.
+
+See <https://docs.beacon3d.com/commands/#beacon_offset_compare>
 
 ## Overview
 
-This module has 2 modes: analysis and compensation.
-
-Analysis mode samples the bed and generates 3 dashboards with various graphs to help diagnose gantry alignment issues by visualizing the Z-offset measurements from Beacon's contact and proximity modes. It leverages the `BEACON_OFFSET_COMPARE` command to calculate deltas across your print bed.
-
-Ideally, the delta between contact and proximity measurements should be zero (or at least consistent) across the bed. Variations indicate that the Beacon probe and nozzle are operating on different planes, leading to probing inaccuracies and first-layer issues.
-
 Compensation mode works out and applies the Z compensation values for Klipper's native gantry twist compensation module `[axis_twist_compensation]`.
 
-### Requirements
+## Requirements
 
 - Klipper
 - Beacon probe
 - SSH access to your printer
-- A printer with internet access (only if using the recommended installation script below)
-
-### Compatibility
-
-This module can work with any printer running Klipper but was designed and tested on a QIDI Plus 4. As such, the installation guide and the default settings are tailored for it. If you install/run on a different printer, please review the installation and settings carefully.
 
 ## Installation
 
-Access your printer via SSH and run:
+Git clone it to your printer and then soft link the beacon_axis_twist_compensation.py to the klipper/klippy/extras directory.
 
-```bash
-cd /home/mks && wget -O - https://raw.githubusercontent.com/omgitsgio/gios_gantry_twist_utility/62a083f4c8356c34b2a8c6e02d26e93530addf10/install.sh | bash
+```
+git clone https://github.com/pellcorp/beacon_axis_twist_compensation.git /usr/data/beacon_axis_twist_compensation
+/usr/data/beacon_axis_twist_compensation/install.sh
 ```
 
 Add the configuration below to your `printer.cfg` **before** the `SAVE_CONFIG` section, then restart Klipper.
 
 ```cfg
-[gantry_twist_utility]
-
-# Operation mode (0 = analysis, 1 = compensation):
-# mode: 0
-
-# graphs_folder: ~/printer_data/config/Gantry_twist_analysis
-
-# Mesh boundaries to probe. When in compensation mode, X values will be used for start_x and end_x.
-# min_x: 22.0
-# max_x: 283.0
-# min_y: 22.0
-# max_y: 283.0
-# calibrate_y: 152.5
-
-# Points per axis (grid_size * grid_size).
-# When in compensation mode, this will be the sample size along X-axis.
-# grid_size: 10
-
-# Test temps
-# bed_temp: 0.0
-# hotend_temp: 0.0
+[beacon_axis_twist_compensation]
+settle_delay: 1.0
+point_delay: 1.0
 ```
-For more configuration options please refer to [sample_config_complete.cfg](sample_config_complete.cfg).
+
+It is expected that you will have defined an `[axis_twist_compensation]` config with at least:
+
+```
+[axis_twist_compensation]
+speed: 50
+horizontal_move_z: 5
+calibrate_start_x: 30
+calibrate_end_x: 220
+calibrate_y: 110
+```
+
+If you wish to do `AXIS=Y`, you would need to add config like the following:
+
+```
+calibrate_start_y: 30
+calibrate_end_y: 220
+calibrate_x: 110
+```
 
 ## Usage
 
-Just send to your console:
-
 ```
-GANTRY_TWIST_UTILITY
-```
-
-If no settings are declared, it will run with the config above by default (hence in analysis mode).
-
-You can specify some arguments from console which will override the config file:
-```
-MODE, BED_TEMP, HOTEND_TEMP, GRID_SIZE, MAX_RETRIES, CALIBRATE_Y
+M104 S150
+G28
+M109 S150
+BEACON_AXIS_TWIST_COMPENSATION SAMPLE_COUNT=3 AXIS=X
 ```
 
-### Compensation Mode
-
-To automatically calculate and apply axis twist compensation values:
-
-```
-GANTRY_TWIST_UTILITY MODE=1
-```
+If no `SAMPLE_COUNT` or `AXIS` is provided, it will run with the config of `3` along `X` axis.
 
 This will:
 1. Sample along the X-axis at the center Y position as per config
@@ -87,75 +67,18 @@ This will:
 3. Update your `[axis_twist_compensation]` configuration
 4. Prompt you to run `SAVE_CONFIG` to persist the changes
 
-### Analysis Mode
-
-This mode will probe the bed as per settings/arguments and run 3 dashboards to help visualize the offset data.
-
-Please note that this is all experimental and it's not an exact science, but it has helped me understand that in my case axis twist compensation would have been sufficient to obtain decent results.
+### Y Axis
 
 
-### Output Interpretation
+```
+M104 S150
+G28
+M109 S150
+BEACON_AXIS_TWIST_COMPENSATION SAMPLE_COUNT=5 AXIS=Y
+```
 
-The analysis generates three visualization images that help you understand your gantry's twist characteristics:
-
-### 1. `beacon_offset_analysis_3d_meshes_overlay`
-Shows overlaid contact and proximity meshes from four different angles, helping you visualize the difference between the nozzle and probe perspectives.
-
-![3D Meshes Overlay](docs/beacon_offset_analysis_3D_meshes_overlay_20251031_234553.png)
-
-### 2. `beacon_offset_analysis_3d_meshes&offset`
-Displays:
-- Two separate mesh graphs (contact and proximity)
-- An overlay view at 45° angle
-- A 3D visualization of the deltas (bottom right)
-
-![3D Meshes & Offset](docs/beacon_offset_analysis_3D_meshes&offset_20251031_234553.png)
-
-### 3. `beacon_offset_analysis`
-The top-left plot immediately reveals patterns in the extent of the delta values, making it easy to identify bias and twist characteristics.
-
-The X and Y Trend Analysis plots on the right display delta values along their respective axes. The script highlights statistically significant trends and ranges exceeding 50µm. In the example below, note the clear sloping trend on the X-axis and the wide ranges on the Y-axis. While this indicates aggressive gantry twist, the consistent, symmetrical X-axis pattern is ideal for Klipper's X-axis compensation.
-
-The bottom graph in the middle groups the measurements by row or column (depending on the sampling direction) and can help confirm offset patterns.
-
-![Beacon Offset Analysis](docs/beacon_offset_analysis_20251031_234553.png)
-
-## Understanding Gantry Twist Impact on First Layer
-
-Gantry twist directly affects first-layer quality by creating inconsistent Z-offsets across the bed. The following are good examples of some patterns that may occur:
-
-### X-Axis Twist Pattern
-When your gantry exhibits twist along the X-axis, you'll see variation in the offset deltas as you move left-to-right across the bed. This is the most common twist pattern on CoreXY printers.
-
-![X-Axis Beacon Offset First Layer](docs/x_beacon_offset_first_layer.png)
-
-### XY Combined Twist Pattern
-More complex twist patterns can occur across both X and Y axes, creating diagonal or corner-biased variations that are harder to compensate for mechanically. Generally speaking, you can reduce or remove the effect of one of the axis twists by choosing a beacon/probe mount with zero X or Y offset.
-
-![XY Combined Beacon Offset First Layer](docs/xy_beacon_offset_first_layer.png)
-
-### X-Axis Twist with Compensation
-When gantry twist compensation is applied, the first layer becomes more consistent, but the underlying mechanical issue remains.
-
-![X-Axis with Gantry Twist Compensation](docs/x_beacon_offset_gTwist_first_layer.png)
-
-## Current Status & Future Plans
-
-### Stability
-The script is reliable with default settings or minor variations of them. However, it hasn't been extensively stress-tested with edge cases and misuse, so be careful if you change the defaults.
-
-### Future Improvements
-The analysis part is very rudimentary. Suggestions are greatly appreciated.
-Potential improvements include:
-- Enhanced console output with twist detection alerts
-- Automated analysis of twist patterns and severity
-- Improved graph layouts and visual storytelling
-- Additional visualization types for clearer data interpretation
+This will generate the new Y axis compensation config
 
 ## Credits
-A lot of the problem-solving was possible by taking inspiration from <https://github.com/Frix-x/klippain-shaketune>.
 
-The install script was mostly a readaptation of <https://github.com/qidi-community/ShakeTune-For-Plus4/blob/cd94df7b4b2ac4f6a56c27dfaf9f40dbff463335/install.sh>.
-
-## Support
-Please refer to the QIDI community on GitHub: <https://github.com/qidi-community> or join the Discord channel: <https://discord.gg/FtDkxKsX>. Feel free to also contact me on GitHub.
+Forked and significantly simplified but based on <https://github.com/omgitsgio/gios_gantry_twist_utility>
---
+++ beacon_axis_twist_compensation.py
@@ -0,0 +1,255 @@
+# Beacon Grid Test - Klipper Module
+#
+# Utility to analyse and apply gantry twist compensation using beacon offset data.
+#
+# Copyright (C) 2025 omgitsgio <gio@omgitsgio.com>
+# Copyright (C) 2025 pellcorp <jason@pellcorp.com>
+#
+# This file may be distributed under the terms of the GNU GPLv3 license.
+
+DEFAULT_SAMPLE_COUNT = 3
+DEFAULT_SPEED = 50.
+DEFAULT_HORIZONTAL_MOVE_Z = 5.
+
+
+class BeaconAxisTwistCompensation:
+    def __init__(self, config):
+        self.printer = config.get_printer()
+        self.configfile = self.printer.lookup_object('configfile')
+        self.compensation = self.printer.lookup_object('axis_twist_compensation')
+        self.gcode = self.printer.lookup_object('gcode')
+        self.name = config.get_name()
+        self.beacon = None
+        self.lift_speed = None
+
+        atconfig = config.getsection('axis_twist_compensation')
+        if atconfig is None:
+            raise self.printer.config_error('BEACON_AXIS_TWIST_COMPENSATION: Missing axis_twist_compensation config')
+
+        self.settle_delay = config.getfloat('settle_delay', 1.0)
+        self.point_delay = config.getfloat('point_delay', 1.0)
+
+        # get values from [axis_twist_compensation] section in printer .cfg
+        self.horizontal_move_z = atconfig.getfloat('horizontal_move_z',
+                                                 DEFAULT_HORIZONTAL_MOVE_Z)
+        self.speed = atconfig.getfloat('speed', DEFAULT_SPEED)
+        self.calibrate_start_x = atconfig.getfloat('calibrate_start_x',
+                                                 default=None)
+        self.calibrate_end_x = atconfig.getfloat('calibrate_end_x', default=None)
+        self.calibrate_y = atconfig.getfloat('calibrate_y', default=None)
+        self.compensation_start_x = atconfig.getfloat('compensation_start_x',
+                                                    default=None)
+        self.compensation_end_x = atconfig.getfloat('compensation_end_x',
+                                                  default=None)
+
+        self.calibrate_start_y = atconfig.getfloat('calibrate_start_y',
+                                                 default=None)
+        self.calibrate_end_y = atconfig.getfloat('calibrate_end_y', default=None)
+        self.calibrate_x = atconfig.getfloat('calibrate_x', default=None)
+        self.compensation_start_y = atconfig.getfloat('compensation_start_y',
+                                                    default=None)
+        self.compensation_end_y = atconfig.getfloat('compensation_end_y',
+                                                  default=None)
+
+        self.x_start_point = (self.calibrate_start_x,
+                              self.calibrate_y)
+        self.x_end_point = (self.calibrate_end_x,
+                            self.calibrate_y)
+        self.y_start_point = (self.calibrate_x,
+                              self.calibrate_start_y)
+        self.y_end_point = (self.calibrate_x,
+                            self.calibrate_end_y)
+
+        self.test_running = False
+        self.results = []
+
+        self.printer.register_event_handler("klippy:connect",
+                                   self._handle_connect)
+
+        self.gcode.register_command(
+            'BEACON_AXIS_TWIST_COMPENSATION',
+            self.cmd_BEACON_AXIS_TWIST_COMPENSATION,
+            desc=self.cmd_BEACON_AXIS_TWIST_COMPENSATION_help
+        )
+
+
+    def _handle_connect(self):
+        config = self.printer.lookup_object('configfile')
+        self.beacon = self.printer.lookup_object('beacon')
+        if self.beacon is None:
+            raise config.error("BEACON_AXIS_TWIST_COMPENSATION: requires a 'beacon' probe")
+        self.toolhead = self.printer.lookup_object('toolhead')
+        self.lift_speed = getattr(self.beacon, 'lift_speed', None)
+
+        self.compare_gcmd = self.gcode.create_gcode_command(
+            "BEACON_OFFSET_COMPARE",
+            "BEACON_OFFSET_COMPARE",
+            {}
+        )
+
+    def _apply_compensations(self, axis):
+        new_z_compensations = self.results
+
+        values_as_str = ', '.join(["{:.6f}".format(x) for x in new_z_compensations])
+        if axis == 'X':
+            self.configfile.set('axis_twist_compensation', 'z_compensations', values_as_str)
+            self.configfile.set('axis_twist_compensation', 'compensation_start_x', self.x_start_point[0])
+            self.configfile.set('axis_twist_compensation', 'compensation_end_x', self.x_end_point[0])
+
+            self.compensation.z_compensations = new_z_compensations
+            self.compensation.compensation_start_x = self.x_start_point[0]
+            self.compensation.compensation_end_x = self.x_end_point[0]
+        else:
+            self.configfile.set('axis_twist_compensation', 'new_zy_compensations', values_as_str)
+            self.configfile.set('axis_twist_compensation', 'compensation_start_y', self.y_start_point[1])
+            self.configfile.set('axis_twist_compensation', 'compensation_end_y', self.y_end_point[1])
+
+            self.compensation.zy_compensations = new_z_compensations
+            self.compensation.compensation_start_y = self.y_start_point[1]
+            self.compensation.compensation_end_y = self.y_end_point[1]
+
+        self.gcode.respond_info(
+            "AXIS_TWIST_COMPENSATION state has been saved "
+            "for the current session.  The SAVE_CONFIG command will "
+            "update the printer config file and restart the printer.")
+        # output result
+        self.gcmd.respond_info(
+            "BEACON_AXIS_TWIST_COMPENSATION: Calibration complete, "
+            "offsets: %s" % self.results)
+
+
+    def _calibration(self, nozzle_points):
+        total_points = len(nozzle_points)
+
+        self.test_running = True
+
+        try:
+            for idx, (x_pos, y_pos) in enumerate(nozzle_points, 1):
+                if not self.test_running:
+                    self.gcmd.respond_info("BEACON_AXIS_TWIST_COMPENSATION cancelled. Exiting...")
+                    break
+
+                self.gcmd.respond_info(f"Point {idx}/{total_points}: X{x_pos:.2f} Y{y_pos:.2f}")
+                
+                # Move to position
+                self._move_helper((x_pos, y_pos, self.horizontal_move_z), self.speed)
+                self.toolhead.wait_moves()
+                self.toolhead.dwell(self.settle_delay)
+
+                self.beacon.cmd_BEACON_OFFSET_COMPARE(self.compare_gcmd)
+                self.toolhead.wait_moves()
+
+                # Collect data from beacon's last_offset_result
+                # Structure: {'position': (x, y, contact_z), 'delta': contact_z - proximity_z}
+                if hasattr(self.beacon, 'last_offset_result') and self.beacon.last_offset_result:
+                    result = self.beacon.last_offset_result
+                    # result format: { 'position': (x, y, contact_z), 'delta': delta }
+                    delta = float(result['delta'])
+                    self.results.append(delta)
+                else:
+                    # it makes no sense to me to allow a partial result its either all or nothing
+                    raise self.gcmd.error("No beacon offset result data available.")
+
+                self.toolhead.dwell(self.point_delay)
+        finally:
+            self.test_running = False
+
+        self.gcmd.respond_info(f"BEACON_AXIS_TWIST_COMPENSATION complete")
+
+
+    # taken straight from axis_twist_compensation
+    def _move_helper(self, target_coordinates, override_speed=None):
+        # pad target coordinates
+        target_coordinates = \
+            (target_coordinates[0], target_coordinates[1], None) \
+                if len(target_coordinates) == 2 else target_coordinates
+        speed = self.speed if target_coordinates[2] == None else self.lift_speed
+        speed = override_speed if override_speed is not None else speed
+        self.toolhead.manual_move(target_coordinates, speed)
+
+
+    cmd_BEACON_AXIS_TWIST_COMPENSATION_help = (
+        "Beacon Axis Twist Compensation using beacon offset."
+    )
+    def cmd_BEACON_AXIS_TWIST_COMPENSATION(self, gcmd):
+        self.gcmd = gcmd
+        sample_count = self.gcmd.get_int('SAMPLE_COUNT', 3, minval=3, maxval=10)
+        axis = self.gcmd.get('AXIS', "X")
+
+        # check for valid sample_count
+        if sample_count < 2:
+            raise self.gcmd.error(
+                "SAMPLE_COUNT to probe must be at least 2")
+
+        if self.test_running:
+            raise self.gcmd.error("BEACON_AXIS_TWIST_COMPENSATION already running")
+
+        mozzle_points = []
+        self.results = []
+
+        if axis == 'X':
+            self.compensation.clear_compensations('X')
+            if not all([
+                self.x_start_point[0],
+                self.x_end_point[0],
+                self.x_start_point[1]
+            ]):
+                raise self.gcmd.error(
+                    """BEACON_AXIS_TWIST_COMPENSATION for X axis requires
+                    calibrate_start_x, calibrate_end_x and calibrate_y
+                    to be defined
+                    """
+                )
+
+            start_point = self.x_start_point
+            end_point = self.x_end_point
+
+            x_axis_range = end_point[0] - start_point[0]
+            interval_dist = x_axis_range / (sample_count - 1)
+
+            for i in range(sample_count):
+                x = start_point[0] + i * interval_dist
+                y = start_point[1]
+                mozzle_points.append((x, y))
+        elif axis == 'Y':
+            self.compensation.clear_compensations('Y')
+            if not all([
+                self.y_start_point[0],
+                self.y_end_point[0],
+                self.y_start_point[1]
+            ]):
+                raise self.gcmd.error(
+                    """BEACON_AXIS_TWIST_COMPENSATION for Y axis requires
+                    calibrate_start_y, calibrate_end_y and calibrate_x
+                    to be defined
+                    """
+                )
+
+            start_point = self.y_start_point
+            end_point = self.y_end_point
+
+            y_axis_range = end_point[1] - start_point[1]
+            interval_dist = y_axis_range / (sample_count - 1)
+
+            for i in range(sample_count):
+                x = start_point[0]
+                y = start_point[1] + i * interval_dist
+                mozzle_points.append((x, y))
+        else:
+            raise self.gcmd.error(
+                "BEACON_AXIS_TWIST_COMPENSATION: "
+                "Invalid axis.")
+
+        try:
+            self._calibration(mozzle_points)
+            if len(self.results) > 0:
+                self._apply_compensations(axis)
+
+        except Exception as e:
+            self.test_running = False
+            raise gcmd.error(f"BEACON_AXIS_TWIST_COMPENSATION: failed: {e}")
+        return
+
+
+def load_config(config):
+    return BeaconAxisTwistCompensation(config)
