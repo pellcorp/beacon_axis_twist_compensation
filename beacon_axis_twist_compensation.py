4e069bbfa90122859158e33cf589c8e5e603d65a
Jason Pell <jason@pellcorp.com>
2025-11-23 15:17:07 +1100

    oops my infinite check was wrong

--- beacon_axis_twist_compensation.py
+++ beacon_axis_twist_compensation.py
@@ -142,7 +142,7 @@ class BeaconAxisTwistCompensation:
                     result = self.beacon.last_offset_result
                     # result format: { 'position': (x, y, contact_z), 'delta': delta }
                     delta = float(result['delta'])
-                    if math.isfinite(delta):
+                    if not math.isfinite(delta):
                         raise self.gcmd.error(f"BEACON_AXIS_TWIST_COMPENSATION: Beacon delta was infinite, perhaps it scanned off the build plate! (Result was: {result['delta']})")
                     self.results.append(delta)
                 else:
